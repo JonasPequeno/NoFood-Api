8e8a040612635c67f543ac0e57b20b6c3d67de07
Jonas Pequeno <Jonaspdasilva31@gmail.com>
2018-12-08 18:07:29 -0300

    Criando modelos: Produto e Categoria

--- noFood.Api/bin/express.js
+++ noFood.Api/bin/express.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const variables = require('./config/variables');
 
 const categoriaRouter = require('../routes/categoria-router');
 const produtoRouter = require('../routes/produto-router');
@@ -12,6 +15,9 @@ const app = express();
 //configuração de parser do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//configuração do mongoose
+mongoose.connect(variables.Database.connection);
 
 app.use('/api/categoria', categoriaRouter);
 app.use('/api/produto', produtoRouter);
